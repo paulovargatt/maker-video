27b082b36b85410adeecb46c19d92c644b4865ef
Paulo Vargatt <pvargatt@gmail.com>
2019-03-20 22:57:19 -0300

    watson

--- robots/text.js
+++ robots/text.js
@@ -3,10 +3,24 @@ const crediantialAlgorithmia = require('../credentials/algorithmia.json');
 const sbd = require('sbd');
 
 
+const watsonApiKey = require('../credentials/watson.json').apikey;
+
+const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
+
+var nlu = new NaturalLanguageUnderstandingV1({
+    iam_apikey: watsonApiKey,
+    version: '2018-04-05',
+    url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
+});
+
+
 async function robot(content) {
     await fetchContentWikipedia(content);
-    await sanitizeContent(content)
-    breakContentIntoSentences(content)
+    await sanitizeContent(content);
+    breakContentIntoSentences(content);
+    limitMaximumSentences(content);
+    await fetchKeywordsOfAllSentences(content);
+
     async function fetchContentWikipedia(content) {
         const algorithmiaAuthenticated = algorithmia(crediantialAlgorithmia.apiKey);
         const wikipediaAlgorithm = algorithmiaAuthenticated.algo("web/WikipediaParser/0.1.2?timeout=300");
@@ -19,6 +33,7 @@ async function robot(content) {
         const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.sourceContentOriginal);
         const withoutDates = removeDates(withoutBlankLinesAndMarkdown)
         content.sourceContentSanitized = withoutDates;
+
         function removeBlankLinesAndMarkdown(text) {
             const allLines = text.split('\n');
             const withoutBlankLinesAndMarkdown = allLines.filter((line) => {
@@ -38,13 +53,42 @@ async function robot(content) {
     function breakContentIntoSentences(content) {
         content.sentences = [];
         const sentences = sbd.sentences(content.sourceContentSanitized);
-       sentences.forEach((sentence) => {
-           content.sentences.push({
-               text: sentence,
-               keywords: [],
-               images: []
-           })
-       })
+        sentences.forEach((sentence) => {
+            content.sentences.push({
+                text: sentence,
+                keywords: [],
+                images: []
+            })
+        })
+    }
+
+    function limitMaximumSentences(content) {
+        content.sentences = content.sentences.slice(0, content.maximumSentences)
+    }
+
+    async function fetchKeywordsOfAllSentences(content) {
+        for (const sentence of content.sentences) {
+            sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text)
+        }
+    }
+
+    async function fetchWatsonAndReturnKeywords(sentence) {
+        return new Promise((resolve, reject) => {
+            nlu.analyze({
+                text: sentence,
+                features: {
+                    keywords: {}
+                }
+            }, (error, response) => {
+                if (error) {
+                    throw  error
+                }
+                const keywords = response.keywords.map((keyword) => {
+                    return keyword.text
+                });
+                resolve(keywords)
+            })
+        })
     }
 }
 
